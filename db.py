bbb7c6d586ad916192e59e9f04785b8618d81ac1
Najad <mrnajadas@gmail.com>
2026-02-19 00:35:05 +0000

    fixed a bug;imported dotenv file to load envfile from os

--- db.py
+++ db.py
@@ -1,7 +1,11 @@
 import os
 from sqlmodel import  create_engine
+from dotenv import load_dotenv
+load_dotenv()  # 👈 THIS  loads the .env file ,I DIDNT KNOW THAT
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print(os.getenv("DATABASE_URL"))
+
 engine = create_engine(DATABASE_URL,echo = True)
 
 
